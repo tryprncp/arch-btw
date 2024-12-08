655d4f2b745951c2ed0d00f5b9880d89eefbeb73
tryprncp <tryprncp@gmail.com>
2024-12-08 10:25:45 +0800

    chore: minimal arch installation with disk encryption

--- arch-btw.sh
+++ arch-btw.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+script installation.log
 set -e
-
-# Choose Arch Linux installation type:
-# 1) Minimal install, 2) With HyDE, 3) With i3WM
-TYPE=""
 
 # Set hostname
 HOSTNAME=""
@@ -19,54 +16,80 @@ USER_PASSWORD=""
 # Set disk name (run lsblk to identify your disk)
 DISK=""
 
-# Set variable based on disk name
+# Set disk encryption passphrase
+PASSPHRASE=""
+
 if [[ $DISK == nvme* ]]; then
-    PART="${DISK}p"
+    PART=${DISK}p
 else
-    PART="${DISK}"
+    PART=${DISK}
 fi
 
 echo -e "\033[0;32m\n[Removing the existing partitions]\033[0m"
-for PART_NUM in $(parted /dev/"$DISK" --script print | awk '/^ / {print $1}'); do
-    parted /dev/"$DISK" --script rm "$PART_NUM"
+for PART_NUM in $(parted /dev/$DISK --script print | awk '/^ / {print $1}'); do
+    parted /dev/$DISK --script rm $PART_NUM
 done
 
 echo -e "\033[0;32m\n[Creating new partitions]\033[0m"
-parted /dev/"$DISK" --script mklabel gpt
-parted /dev/"$DISK" --script mkpart primary fat32 1MiB 500MiB
-parted /dev/"$DISK" --script set 1 esp on
-parted /dev/"$DISK" --script mkpart primary ext4 500MiB 100%
-echo -e "\033[0;32m\n[Formatting partition 1]\033[0m"
-mkfs.fat -F 32 /dev/"${PART}"1
-echo -e "\033[0;32m\n[Formatting partition 2]\033[0m"
-mkfs.ext4 /dev/"${PART}"2
+parted /dev/$DISK --script mklabel gpt
+parted /dev/$DISK --script mkpart primary fat32 1MiB 500MiB
+parted /dev/$DISK --script set 1 esp on
+parted /dev/$DISK --script mkpart primary ext4 500MiB 1500MiB
+parted /dev/$DISK --script mkpart primary luks 1500MiB 100%
 
-echo -e "\033[0;32m\n[Mounting the root partition to /mnt]\033[0m"
-mount /dev/"${PART}"2 /mnt
+echo -e "\033[0;32m\n[Formatting efi partition]\033[0m"
+mkfs.fat -F 32 /dev/${PART}1
+echo -e "\033[0;32m\n[Formatting boot partition]\033[0m"
+mkfs.ext4 /dev/${PART}2
+
+echo -e "\033[0;32m\n[Setting up encrypted partition]\033[0m"
+echo $PASSPHRASE | cryptsetup -v -q -s 512 -h sha512 luksFormat /dev/${PART}3
+echo $PASSPHRASE | cryptsetup luksOpen /dev/${PART}3 disknuts
+
+echo -e "\033[0;32m\n[Creating LVM partitions]\033[0m"
+pvcreate /dev/mapper/disknuts
+vgcreate arch /dev/mapper/disknuts
+lvcreate --size 32g arch --name swap
+lvcreate -l 100%FREE arch --name root
+vgreduce --size -256M arch/root
+
+echo -e "\033[0;32m\n[Formatting LVM partitions]\033[0m"
+mkswap /dev/arch/swap
+mkfs.ext4 /dev/arch/root
+
+echo -e "\033[0;32m\n[Mounting the filesystems]\033[0m"
+mount /dev/arch/root /mnt
+mount --mkdir /dev/${PART}2 /mnt/boot
+mount --mkdir /dev/${PART}1 /mnt/boot/efi
+swapon /dev/arch/swap
 
 echo -e "\033[0;32m\n[Initializing pacman-key]\033[0m"
 pacman-key --init
 pacman-key --populate archlinux
 
 echo -e "\033[0;32m\n[Setting up mirrorlist]\033[0m"
-reflector -c "SG" -f 10 -l 10 -n 10 --save /etc/pacman.d/mirrorlist
+reflector -c SG -f 10 -l 10 --save /etc/pacman.d/mirrorlist
 
 echo -e "\033[0;32m\n[Installing the base system]\033[0m"
-for i in {1..10}; do pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware intel-ucode && break; done
+for i in {1..5}; do
+    pacstrap -K /mnt base intel-ucode linux linux-firmware sof-firmware && break
+done
 
 echo -e "\033[0;32m\n[Generating filesystem table]\033[0m"
-genfstab -U /mnt >/mnt/etc/fstab
+genfstab -U /mnt > /mnt/etc/fstab
 
 echo -e "\033[0;32m\n[Creating a chroot script]\033[0m"
-cat <<EOF_CHROOT >/mnt/root/chroot_script.sh
-#!/bin/bash
+cat << MEOW > chroot_script.sh
 set -e
 
 echo -e "\033[0;32m\n[Installing essential packages]\033[0m"
-for i in {i..10}; do pacman -S --needed --noconfirm git grub efibootmgr neovim networkmanager man-db sudo && break; done
+for i in {1..5}; do
+    pacman -S --needed --noconfirm base-devel efibootmgr git grub lvm2 man-db neovim networkmanager sudo zsh && break
+done
 
 echo -e "\033[0;32m\n[Setting up timezone]\033[0m"
 ln -sf /usr/share/zoneinfo/Asia/Manila /etc/localtime
+sed -i 's/#NTP=/NTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org/' /etc/systemd/timesyncd.conf
 hwclock --systohc
 systemctl enable systemd-timesyncd
 systemctl start systemd-timesyncd
@@ -75,66 +98,56 @@ echo -e "\033[0;32m\n[Setting up language and locale]\033[0m"
 sed -i 's/#en_PH.UTF-8 UTF-8/en_PH.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo LANG=en_PH.UTF-8 > /etc/locale.conf
-export LANG=en_PH.UTF-8
 
 echo -e "\033[0;32m\n[Setting up hostname]\033[0m"
 echo $HOSTNAME > /etc/hostname
-cat <<HOSTS > /etc/hosts
-127.0.0.1  localhost
-::1  localhost
-127.0.0.1  $HOSTNAME
-HOSTS
+cat << PURR > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.0.1   $HOSTNAME
+PURR
 
 echo -e "\033[0;32m\n[Installing grub on partition 1]\033[0m"
-mount --mkdir /dev/${PART}1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+UUID=$(blkid -s UUID -o value /dev/${PART}3)
+sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/quiet/quiet root=\/dev\/mapper\/arch-root cryptdevice=UUID=$UUID:disknuts/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo -e "\033[0;32m\n[Configuring system to unlock the partition with a keyfile]\033[0m"
+mkdir /secure
+dd if=/dev/random of=/secure/root_keyfile.bin bs=512 count=2
+cryptsetup luksAddKey /dev/${PART}3 /secure/root_keyfile.bin
+sed -i '/^HOOKS=/ s/block /block encrypt lvm2 /' /etc/mkinitcpio.conf
+sed -i '/^FILES=/ s/()/(\/secure\/root_keyfile.bin)/' /etc/mkinitcpio.conf
+
 echo -e "\033[0;32m\n[Setting up root and user credentials]\033[0m"
-echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd
-useradd -m $USERNAME
-echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USERNAME
-usermod -aG wheel,audio,video,storage $USERNAME
+echo "root:$ROOT_PASSWORD" | chpasswd
+useradd -m -G audio,video,storage -s $(which zsh) $USERNAME
+echo "$USERNAME:$USER_PASSWORD" | chpasswd
 echo "$USERNAME ALL=(ALL) ALL" > /etc/sudoers.d/$USERNAME
 
-# Ignore power button
-sed -i 's/^#HandlePowerKey=poweroff/HandlePowerKey=ignore/' /etc/systemd/logind.conf
-
-echo -e "\033[0;32m\n[Enabling services]\033[0m"
+echo -e "\033[0;32m\n[Enable networking]\033[0m"
 systemctl enable NetworkManager
-EOF_CHROOT
 
-# Append additional commands to chroot_script.sh to install HyDE if the $TYPE is 2
-if [ "$TYPE" == "2" ]; then
-    echo -e "\033[0;32m\n[Installing HyDE]\033[0m"
-    echo "su - $USERNAME -c '{
-        git clone --depth 1 https://github.com/tryprncp/hyprdots HyDE
-        ./HyDE/Scripts/install.sh
-    }'" >>/mnt/root/chroot_script.sh
-fi
-
-# Append additional commands to chroot_script.sh to install i3WM if the $TYPE is 3
-if [ "$TYPE" == "3" ]; then
-    echo -e "\033[0;32m\n[Installing i3WM]\033[0m"
-    echo "su - $USERNAME -c '{
-        git clone --depth 1 https://github.com/tryprncp/i3WM
-        ./i3WM/Scripts/install.sh
-    }'" >>/mnt/root/chroot_script.sh
-fi
+echo -e "\033[0;32m\n[Exiting arch-chroot environment]\033[0m"
+MEOW
 
 echo -e "\033[0;32m\n[Entering arch-chroot environment]\033[0m"
 arch-chroot /mnt /bin/bash /root/chroot_script.sh
-echo -e "\033[0;32m\n[Exiting arch-chroot environment]\033[0m"
-echo -e "\033[0;32m\n[Removing chroot script]\033[0m"
-rm /mnt/root/chroot_script.sh
 
-echo -e "\033[0;32m\n[Unmounting the root partition]\033[0m"
+echo -e "\033[0;32m\n[Cleaning up]\033[0m"
+cp installation.log /mnt/root/installation.log
+rm /mnt/root/chroot_script.sh
+exit
+
+echo -e "\033[0;32m\n[Unmounting the filesystems]\033[0m"
+swapoff -a
 umount -l /mnt
 
-# Execute shutdown if everything is successful
 if [ $? -eq 0 ]; then
     echo -e "\033[0;32m\n[I use Arch, btw]\033[0m"
-    shutdown now
+    sleep 3
+    systemctl reboot
 else
     exit 2
 fi
